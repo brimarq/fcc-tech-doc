5254ee026b21bf0b4bfd18de295d04f58dc3df66
Brian Marquis <rbmarquis.dev@gmail.com>
2019-10-23 22:02:01 -0500

    Move nav-link click listeners & handler

--- src/script.js
+++ src/script.js
@@ -1,5 +1,15 @@
-// Close mobile dropdown menu on orientation change
-window.addEventListener("orientationchange", function() {
-  document.querySelector("input#hamburger-toggle").checked = false;
-});
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerToggle = document.querySelector("input#hamburger-toggle");
+  const navLinks = document.querySelectorAll("a.nav-link");
+  
+  // Close mobile dropdown menu on orientation change
+  window.addEventListener("orientationchange", () => hamburgerToggle.checked = false);
 
+  navLinks.forEach(link => link.addEventListener("click", linkClickHandler));
+
+  function linkClickHandler() {
+    hamburgerToggle.checked = false;
+    navLinks.forEach(link => link.classList.remove("is-active"));
+    this.classList.add("is-active");
+  };
+});
