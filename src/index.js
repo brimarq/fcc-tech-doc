2909a06fc9c3cd08ae19ee8bbf26120157a41e98
Brian Marquis <rbmarquis.dev@gmail.com>
2019-11-01 20:11:00 -0500

    Enable HMR for HTML with morphdom

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import Prism from 'prismjs';
-import 'normalize.css/normalize.css';
-import 'prismjs/plugins/diff-highlight/prism-diff-highlight.css';
-import 'prismjs/plugins/diff-highlight/prism-diff-highlight.js';
+import morphdom from 'morphdom';
+import { default as pugToHtmlStr } from './index.pug';
 
-import './style.css';
+import Prism from 'prismjs';
+import 'prismjs/components/prism-pug.js';
+import 'prismjs/components/prism-json.js';
+import 'prismjs/components/prism-bash.js';
+import 'prismjs/components/prism-scss.js';
 import './script.js';
 
+import 'normalize.css';
+// import 'prismjs/themes/prism-dark.css';
+// import 'prismjs/themes/prism-okaidia.css';
+// import 'prismjs/themes/prism-solarizedlight.css'; // 1
+import 'prismjs/themes/prism.css';
+import './style.css';
 
-// Inject compiled index.pug content as HTML
 (() => {
-  const root = document.getElementById('root');
-  return root.innerHTML = require('./index.pug')();
+  let root = document.getElementById('root'); 
+  let newRoot = document.createRange().createContextualFragment(
+    `<div id="root">${pugToHtmlStr()}</div>`
+  );
+  let updatedRoot = morphdom(root, newRoot, {
+    onBeforeElUpdated: (fromEl, toEl) => (
+      fromEl.isEqualNode(toEl) ? false : true
+    ),
+    childrenOnly: true
+  });
+  return updatedRoot;
 })();
 
 if (Prism) Prism.highlightAll();
