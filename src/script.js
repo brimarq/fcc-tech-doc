29bac79795c28d12ba74b772df00c3d6d6d230e4
Brian Marquis <rbmarquis.dev@gmail.com>
2019-11-25 22:24:34 -0600

    update, code cleanup

--- src/script.js
+++ src/script.js
@@ -1,14 +1,73 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const hamburgerToggle = document.querySelector("input#hamburger-toggle");
+  const burgerToggle = document.querySelector("input#burger-toggle");
   const navLinks = document.querySelectorAll("a.nav-link");
   
   // Close mobile dropdown menu on orientation change
-  window.addEventListener("orientationchange", () => hamburgerToggle.checked = false);
+  window.addEventListener("orientationchange", () => burgerToggle.checked = false);
   navLinks.forEach(link => link.addEventListener("click", linkClickHandler));
 
-  function linkClickHandler() {
-    hamburgerToggle.checked = false;
+  useCustomFccToggler();
+
+
+  
+
+  async function useCustomFccToggler() {
+    const fccTestSuite = document.querySelector("#fcc_test_suite_wrapper");
+    const newToggler = document.querySelector("#custom_fcc_foldout_toggler");
+
+    if (!fccTestSuite) return console.log("*** FCC TEST SUITE NOT LOADED ***");
+    if (!newToggler) return console.log("*** MISSING ELEMENT: #custom_fcc_foldout_toggler ***");
+
+    try {
+      const queryResults = await querySelectorAllPromise('#fcc_toggle, #fcc_foldout_toggler, #fcc_foldout_menu', fccTestSuite.shadowRoot);
+
+      const fccToggle = queryResults.find(el => el.id === 'fcc_toggle');
+      const fccToggler = queryResults.find(el => el.id === 'fcc_foldout_toggler');
+      const fccMenu = queryResults.find(el => el.id === 'fcc_foldout_menu');
+
+      // Hide original toggler & toggle
+      if (fccToggler) fccToggler.style.display = 'none';
+      if (fccToggle) fccToggle.style.display = 'none';
+      // Close test menu if it's open by default
+      if (fccMenu && fccToggle && fccMenu.getBoundingClientRect().left >= 0) toggleCheckbox(fccToggle);
+
+      return newToggler.addEventListener('click', newTogglerClickHandler);
+
+      function toggleCheckbox(checkbox) { return checkbox.checked ^= 1; }
+
+      function newTogglerClickHandler() {
+        toggleCheckbox(fccToggle);
+        return this.classList.toggle('is-open');
+      }
+
+      function querySelectorAllPromise(selector, parentNode = document) {
+        return new Promise((resolve, reject) => {
+          let result = parentNode.querySelectorAll(selector);
+  
+          if (result.length) {
+            result.length > 1 ? resolve(Array.from(result)) : resolve(result[0]);
+          }
+  
+          new MutationObserver((mutationRecords, observer) => {
+            result = parentNode.querySelectorAll(selector);
+            if (result.length) observer.disconnect();
+            result.length > 1 ? resolve(Array.from(result)) : resolve(result[0]);
+          }).observe(parentNode, {childList: true, subtree: true }); 
+    
+        });
+      }
+      
+    } catch(err) {
+      console.error(err);
+    }
+    
+  }
+
+  function linkClickHandler(e) {
+    burgerToggle.checked = false;
+    console.log(e.target);
     navLinks.forEach(link => link.classList.remove("is-active"));
     this.classList.add("is-active");
   };
+
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,54 +2,11 @@ const path = require('path');
 const MiniHtmlWebpackPlugin = require('mini-html-webpack-plugin');
 const miniHtmlWebpackPluginTemplate = require('@vxna/mini-html-webpack-template');
 const postCssUse = require('postcss-use');
-const autoprefixer = require('autoprefixer');
+// const autoprefixer = require('autoprefixer');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpackDashboard = require('webpack-dashboard/plugin');
 
-const cdn = {
-  fccTestSuite: { script: { src: "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js" } },
-  normalize: { link: { rel: "stylesheet", href: "https://unpkg.com/normalize.css@8.0.1/normalize.css"} },
-  bulma: { link: { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css" } },
-  fontAwesome: { script: { defer: true, src: "https://use.fontawesome.com/releases/v5.3.1/js/all.js" } },
-  prismjs: {
-    link: {
-      themes: {
-        dark: { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/themes/prism-dark.min.css" },
-        prism: { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/themes/prism.min.css" }
-      }
-    },
-    script: {
-      prism: { src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/prism.min.js"},
-      languages: {
-        pug: { src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-pug.min.js"},
-        json: { src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-json.min.js"},
-        bash: { src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-bash.min.js"},
-        scss: { src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-scss.min.js"}
-      }
-    },
-  }
-};
-
-const codePenPostCssUseModules = [
-  'lost',
-  'postcss-apply',
-  'postcss-color-function',
-  'postcss-conditionals',
-  'postcss-custom-media',
-  'postcss-discard-comments',
-  'postcss-each',
-  'postcss-extend',
-  'postcss-flexbox',
-  'postcss-for',
-  'postcss-media-minmax',
-  'postcss-mixins',
-  'postcss-nested',
-  'postcss-nested-ancestors',
-  'postcss-preset-env',
-  'postcss-reverse-media',
-  'postcss-simple-vars',
-  'postcss-triangle'
-];
+const codePenPostCssUseModules = ['lost', 'postcss-apply', 'postcss-color-function', 'postcss-conditionals', 'postcss-custom-media', 'postcss-discard-comments', 'postcss-each', 'postcss-extend', 'postcss-flexbox', 'postcss-for', 'postcss-media-minmax', 'postcss-mixins', 'postcss-nested', 'postcss-nested-ancestors', 'postcss-preset-env', 'postcss-reverse-media', 'postcss-simple-vars', 'postcss-triangle'];
 
 module.exports = {
   mode: 'development',
@@ -90,7 +47,7 @@ module.exports = {
             options: {
               plugins: () => [
                 postCssUse({ modules: codePenPostCssUseModules }),
-                autoprefixer()
+                // autoprefixer()
               ]
             }
           }
@@ -108,19 +65,20 @@ module.exports = {
         container: 'root',
         head: {
           links: [
-            // cdn.normalize.link,
-            // cdn.prismjs.link.themes.prism
+            { rel: "stylesheet", href: "https://unpkg.com/normalize.css@8.0.1/normalize.css"},
+            { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Lato|Montserrat:800&display=swap" },
+            { rel: "stylesheet", href: "https://unpkg.com/prism-themes@1.3.0/themes/prism-material-dark.css" }
           ],
           scripts: []
         },
         body: {
           scripts: [ 
-            // cdn.prismjs.script.prism,
-            // cdn.prismjs.script.languages.pug,
-            // cdn.prismjs.script.languages.json,
-            // cdn.prismjs.script.languages.bash,
-            // cdn.prismjs.script.languages.scss,
-            cdn.fccTestSuite.script
+            { src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/prism.min.js"},
+            { src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-pug.min.js"},
+            { src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-json.min.js"},
+            { src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-bash.min.js"},
+            { src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-scss.min.js"},
+            { src: "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js" }
           ]
         },
         trimWhitespace: true
