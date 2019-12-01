439e0bfd7b2865c81bdd65893534f1691506cb36
Brian Marquis <rbmarquis.dev@gmail.com>
2019-11-30 23:50:04 -0600

    code cleanup

--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
 import morphdom from 'morphdom';
 import { default as pugToHtmlStr } from './index.pug';
-
-// import Prism from 'prismjs';
-// import 'prismjs/components/prism-pug.js';
-// import 'prismjs/components/prism-json.js';
-// import 'prismjs/components/prism-bash.js';
-// import 'prismjs/components/prism-scss.js';
-import './script.js';
-
-// import 'normalize.css';
-
-/** PrismJS builtin themes */
-// import 'prismjs/themes/prism-dark.css';
-// import 'prismjs/themes/prism-okaidia.css';
-// import 'prismjs/themes/prism-twilight.css';
-// import 'prismjs/themes/prism-solarizedlight.css'; // 1
-// import 'prismjs/themes/prism.css';
-
-/** prism-themes for PrismJS */
-// import 'prism-themes/themes/prism-atom-dark.css';
-// import 'prism-themes/themes/prism-material-dark.css'; // 1
-// import 'prism-themes/themes/prism-a11y-dark.css';
-// import 'prism-themes/themes/prism-dracula.css';
-
-
-// import './prism-cpentwilight.css';
 import './style.css';
+import './script.js';
 
 (() => {
   let root = document.getElementById('root'); 
